9ad53b3457a2aa99add2c70cdb064331bfc7b1c2
mckunkel <mkunkel@jlab.org>
2019-01-28 16:24:26 -0500

    fixed bug in run_script.py

--- data_stuff/run_scripts.py
+++ data_stuff/run_scripts.py
@@ -1,7 +1,6 @@
 from get_data import run_data_checker
 from clean_data import clean_data
-from create_decoder import  create_decoder
-from split_save_data import split_save
+from split_save_data import  run_package
 
 #in this order
 # 1. Check if data exists. If not get data
@@ -9,7 +8,9 @@ from split_save_data import split_save
 # 3. Create the decoder
 # 4. Create the training.csv files
 
+print('Checking for data')
 run_data_checker()
+print('Cleaning for data')
 clean_data()
-create_decoder()
-split_save()
+print('Running decoder and split save decoder')
+run_package()
--- data_stuff/split_save_data.py
+++ data_stuff/split_save_data.py
@@ -1,11 +1,21 @@
 from create_dataframe import create_dataframe
+import numpy as np
 
+
+meta_data = create_dataframe()
 def split_save():
     # 25 images per class
-    meta_data = create_dataframe()
     value = meta_data.groupby('category_name', group_keys=False).apply(lambda x: x.sample(n=25, replace=False))
     value.sort_index(inplace=True)
     meta_data.drop(value.index, axis=0, inplace=True)
 
     meta_data.to_csv('train_metadata.csv', index=False)
     value.to_csv('val_metadata.csv', index=False)
+
+def create_decoder():
+    decode = {n: i for i, n in meta_data.groupby('category_name').category_number.first().iteritems()}
+    np.save('decode.npy', decode)
+
+def run_package():
+    split_save()
+    create_decoder()
