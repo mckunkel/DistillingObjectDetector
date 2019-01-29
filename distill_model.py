85a85f94e130d9536e1aaa7e308bc89f5e539d4e
mckunkel <mkunkel@jlab.org>
2019-01-29 17:45:39 -0500

    attempt training miniXception p6

--- distill_model.py
+++ distill_model.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from keras import optimizers
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
@@ -53,121 +54,115 @@ val_generator = data_generator.flow_from_directory(
     batch_size=16
 )
 
-# class Distill:
-#     def __init__(self, temperature, lambda_constant, teacher_model, student_model):
-#         self.temperature = temperature
-#         self.lambda_constant = lambda_constant
-#         self.teacher_model = teacher_model
-#         self.student_model = student_model
-#
-#         # Assuming that last layer is softmax remove softmax
-#         self.student_model.layers.pop()
+def distill(temperature = 5.0, lambda_const = 0.07):
+    model = miniXception(weight_decay=1e-5, num_residuals=0)
+    # remove softmax
+    model.layers.pop()
 
-temperature = 5.0
+    # usual probabilities
+    logits = model.layers[-1].output
+    probabilities = Activation('softmax')(logits)
 
-# model = get_mobilenet(224, alpha=0.25, weight_decay=1e-5, dropout=0.1)
-model = miniXception(weight_decay=1e-5, num_residuals=0)
-# remove softmax
-model.layers.pop()
+    # softed probabilities
+    logits_T = Lambda(lambda x: x / temperature)(logits)
+    probabilities_T = Activation('softmax')(logits_T)
 
-# usual probabilities
-logits = model.layers[-1].output
-probabilities = Activation('softmax')(logits)
+    output = concatenate([probabilities, probabilities_T])
+    model = Model(model.input, output)
 
-# softed probabilities
-logits_T = Lambda(lambda x: x/temperature)(logits)
-probabilities_T = Activation('softmax')(logits_T)
+    # now model outputs 512 dimensional vectors
 
-output = concatenate([probabilities, probabilities_T])
-model = Model(model.input, output)
-# now model outputs 512 dimensional vectors
+    # custom loss function
+    def knowledge_distillation_loss(y_true, y_pred, lambda_const):
+        # split in
+        #    onehot hard true targets
+        #    logits from xception
+        y_true, logits = y_true[:, :256], y_true[:, 256:]
 
-# custom loss function
-def knowledge_distillation_loss(y_true, y_pred, lambda_const):
-    # split in
-    #    onehot hard true targets
-    #    logits from xception
-    y_true, logits = y_true[:, :256], y_true[:, 256:]
+        # convert logits to soft targets
+        y_soft = K.softmax(logits / temperature)
 
-    # convert logits to soft targets
-    y_soft = K.softmax(logits / temperature)
+        # split in
+        #    usual output probabilities
+        #    probabilities made softer with temperature
+        y_pred, y_pred_soft = y_pred[:, :256], y_pred[:, 256:]
 
-    # split in
-    #    usual output probabilities
-    #    probabilities made softer with temperature
-    y_pred, y_pred_soft = y_pred[:, :256], y_pred[:, 256:]
+        return lambda_const * logloss(y_true, y_pred) + logloss(y_soft, y_pred_soft)
 
-    return lambda_const * logloss(y_true, y_pred) + logloss(y_soft, y_pred_soft)
+    # For testing use usual output probabilities (without temperature)
 
-# For testing use usual output probabilities (without temperature)
+    def accuracy(y_true, y_pred):
+        y_true = y_true[:, :256]
+        y_pred = y_pred[:, :256]
+        return categorical_accuracy(y_true, y_pred)
 
-def accuracy(y_true, y_pred):
-    y_true = y_true[:, :256]
-    y_pred = y_pred[:, :256]
-    return categorical_accuracy(y_true, y_pred)
+    def top_5_accuracy(y_true, y_pred):
+        y_true = y_true[:, :256]
+        y_pred = y_pred[:, :256]
+        return top_k_categorical_accuracy(y_true, y_pred)
 
-def top_5_accuracy(y_true, y_pred):
-    y_true = y_true[:, :256]
-    y_pred = y_pred[:, :256]
-    return top_k_categorical_accuracy(y_true, y_pred)
+    def categorical_crossentropy(y_true, y_pred):
+        y_true = y_true[:, :256]
+        y_pred = y_pred[:, :256]
+        return logloss(y_true, y_pred)
 
-def categorical_crossentropy(y_true, y_pred):
-    y_true = y_true[:, :256]
-    y_pred = y_pred[:, :256]
-    return logloss(y_true, y_pred)
+    # logloss with only soft probabilities and targets
+    def soft_logloss(y_true, y_pred):
+        logits = y_true[:, 256:]
+        y_soft = K.softmax(logits / temperature)
+        y_pred_soft = y_pred[:, 256:]
+        return logloss(y_soft, y_pred_soft)
 
-# logloss with only soft probabilities and targets
-def soft_logloss(y_true, y_pred):
-    logits = y_true[:, 256:]
-    y_soft = K.softmax(logits/temperature)
-    y_pred_soft = y_pred[:, 256:]
-    return logloss(y_soft, y_pred_soft)
+    # Train student model
 
-# Train student model
-lambda_const = 0.07
+    model.compile(
+        optimizer=optimizers.SGD(lr=1e-2, momentum=0.9, nesterov=True),
+        loss=lambda y_true, y_pred: knowledge_distillation_loss(y_true, y_pred, lambda_const),
+        metrics=[accuracy, top_5_accuracy, categorical_crossentropy, soft_logloss]
+    )
 
-model.compile(
-    optimizer=optimizers.SGD(lr=1e-2, momentum=0.9, nesterov=True),
-    loss=lambda y_true, y_pred: knowledge_distillation_loss(y_true, y_pred, lambda_const),
-    metrics=[accuracy, top_5_accuracy, categorical_crossentropy, soft_logloss]
-)
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=400, epochs=30, verbose=1,
+        callbacks=[
+            ReduceLROnPlateau(monitor='val_acc', factor=0.1, patience=2, min_delta=0.007),
+            EarlyStopping(monitor='val_acc', patience=4, min_delta=0.01)
+        ],
+        validation_data=val_generator, validation_steps=80, workers=4
+    )
 
-model.fit_generator(
-    train_generator,
-    steps_per_epoch=400, epochs=30, verbose=1,
-    callbacks=[
-        ReduceLROnPlateau(monitor='val_acc', factor=0.1, patience=2, min_delta=0.007),
-        EarlyStopping(monitor='val_acc', patience=4, min_delta=0.01)
-    ],
-    validation_data=val_generator, validation_steps=80, workers=4
-)
+    # metric plots
+    plt.plot(model.history.history['categorical_crossentropy'], label='train')
+    plt.plot(model.history.history['val_categorical_crossentropy'], label='val')
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('logloss')
+    plt.savefig('student_logloss_vs_epoch.png')
 
-# metric plots
-plt.plot(model.history.history['categorical_crossentropy'], label='train')
-plt.plot(model.history.history['val_categorical_crossentropy'], label='val')
-plt.legend()
-plt.xlabel('epoch')
-plt.ylabel('logloss')
-plt.savefig('student_logloss_vs_epoch.png')
+    plt.plot(model.history.history['acc'], label='train')
+    plt.plot(model.history.history['val_acc'], label='val')
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.savefig('student_accuracy_vs_epoch.png')
 
-plt.plot(model.history.history['acc'], label='train')
-plt.plot(model.history.history['val_acc'], label='val')
-plt.legend()
-plt.xlabel('epoch')
-plt.ylabel('accuracy')
-plt.savefig('student_accuracy_vs_epoch.png')
+    plt.plot(model.history.history['top_5_accuracy'], label='train')
+    plt.plot(model.history.history['val_top_5_accuracy'], label='val')
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('top5_accuracy')
+    plt.savefig('student_top5_accuracy_vs_epoch.png')
 
-plt.plot(model.history.history['top_5_accuracy'], label='train')
-plt.plot(model.history.history['val_top_5_accuracy'], label='val')
-plt.legend()
-plt.xlabel('epoch')
-plt.ylabel('top5_accuracy')
-plt.savefig('student_top5_accuracy_vs_epoch.png')
+    val_generator_no_shuffle = data_generator.flow_from_directory(
+        data_dir + 'val_no_resizing', val_logits,
+        target_size=(299, 299),
+        batch_size=16, shuffle=False
+    )
+    model.save_weights('miniXception_weights.hdf5')
+    print(model.evaluate_generator(val_generator_no_shuffle, 80))
 
-val_generator_no_shuffle = data_generator.flow_from_directory(
-    data_dir + 'val_no_resizing', val_logits,
-    target_size=(224, 224),
-    batch_size=64, shuffle=False
-)
-model.save_weights('miniXception_weights.hdf5')
-print(model.evaluate_generator(val_generator_no_shuffle, 80))
+
+if __name__ == '__main__':
+    temperature = sys.argv[1]
+    lambda_const = sys.argv[2]
+    distill(temperature, lambda_const)
