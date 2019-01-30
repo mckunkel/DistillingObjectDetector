f12dcb54773c98c927b401765968a73e2f09717f
mckunkel <mkunkel@jlab.org>
2019-01-30 17:04:17 -0500

    attempt training miniXception p30

--- run_distillation_at_value.py
+++ run_distillation_at_value.py
@@ -6,4 +6,10 @@ lamdas=[0.02, 0.2, 0.5, 1]
 list3=[(x,y) for x in temps for y in lamdas]
 
 for temperature,lambda_constant in list3:
+    print('################ Temperature ################')
+    print('################     {}      ################'.format(temperature))
+
+    print('################ Lambda Constant ################')
+    print('################     {}      ################'.format(lambda_constant))
+
     ds(temperature, lambda_constant)
