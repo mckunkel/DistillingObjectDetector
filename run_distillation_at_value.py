696a95c21d618abcd62c4070dc890b592d47dea3
mckunkel <mkunkel@jlab.org>
2019-02-04 20:10:41 -0500

    attempt training miniXception p46

--- run_distillation_at_value.py
+++ run_distillation_at_value.py
@@ -22,7 +22,7 @@ def distill_Xception(part):
     if part == 1:
         temps = [2.5, 5]
         lamdas = [0.02, 0.2, 0.5, 1]
-        residuals = range(7)
+        residuals = range(3)
         list3 = [(x, y, z) for x in temps for y in lamdas for z in residuals]
         for temperature, lambda_constant, residual in list3:
             if check_file('miniXception',temperature, lambda_constant, residual):
@@ -32,7 +32,7 @@ def distill_Xception(part):
     elif part == 2:
         temps = [10, 15]
         lamdas = [0.02, 0.2, 0.5, 1]
-        residuals = range(7)
+        residuals = range(3)
         list3 = [(x, y, z) for x in temps for y in lamdas for z in residuals]
         for temperature, lambda_constant, residual in list3:
             if check_file('miniXception',temperature, lambda_constant, residual):
